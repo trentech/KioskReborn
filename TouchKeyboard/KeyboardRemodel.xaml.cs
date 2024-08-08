26867bedcef85083958fef293c9c8982b3cd6e9c
trentech <terrence.monroe@proton.me>
2024-08-08 17:21:04 -0400

    Fix keyboard not referencing library

--- TouchKeyboard/KeyboardRemodel.xaml.cs
+++ TouchKeyboard/KeyboardRemodel.xaml.cs
@@ -1,21 +1,11 @@
-﻿using KioskReborn;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using WindowsInput;
 using WindowsInput.Native;
+using KioskRebornLib;
 
 namespace TouchKeyboard
 {
